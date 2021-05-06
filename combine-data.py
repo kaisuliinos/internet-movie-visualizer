0569a72d358ab31c8442334990a83789f1e5371b
Mika Kuitunen <mika.kuitunen@hotmail.com>
2021-05-06 16:56:56 +0300

    Add data comining script

--- combine-data.py
+++ combine-data.py
@@ -1,28 +1,27 @@
+import numpy as np
 import pandas as pd
 
-env = 'prod' # 'test' or 'prod'
+env = 'test' # 'test' or 'prod'
 
 def line():
   print('\n')
 
-def formPaths(file):
+def formPath(file):
   data_path = 'data/'
   extension = '.tsv'
   suffix = '.{}'.format(env)
 
-  input = '{}{}{}'.format(data_path, file, extension)
-  output = '{}{}{}{}'.format(data_path, file, suffix, extension)
-  return (input, output)
+  return '{}{}{}{}'.format(data_path, file, suffix, extension)
 
 titles_file = 'title.basics'
 principals_file = 'title.principals'
 names_file = 'name.basics'
 ratings_file = 'title.ratings'
 
-titles_input, titles_output = formPaths(titles_file)
-principals_input, principals_output = formPaths(principals_file)
-names_input, names_output = formPaths(names_file)
-ratings_input, ratings_output = formPaths(ratings_file)
+titles_input = formPath(titles_file)
+principals_input = formPath(principals_file)
+names_input = formPath(names_file)
+ratings_input = formPath(ratings_file)
 
 # -------------------------------------------------------------------------------------------------
 # Titles
@@ -48,18 +47,8 @@ print('Title types:')
 print(titles.titleType.unique())
 line()
 
-categories = ['movie', 'tvMovie', 'tvSeries', 'tvMiniSeries']
-titles = titles[titles['titleType'].isin(categories)]
-
-if (env == 'test'):
-  titles = titles.sample(n=10000, random_state=0)
-
-titles.reset_index(drop=True, inplace=True)
-
 print(titles.head(5))
 line()
-
-titles.to_csv(titles_output, sep='\t', na_rep='\\N', index=False, line_terminator='\n')
 
 # -------------------------------------------------------------------------------------------------
 # Principals
@@ -84,17 +73,8 @@ print('Principal categories:')
 print(principals.category.unique())
 line()
 
-job_titles = ['self', 'director', 'cinematographer', 'composer', 'producer', 'actor', 'actress', 'writer']
-
-principals = principals[principals['category'].isin(job_titles)]
-principals = principals[principals['tconst'].isin(title_ids)]
-
-principals.reset_index(drop=True, inplace=True)
-
 print(principals.head(5))
 line()
-
-principals.to_csv(principals_output, sep='\t', na_rep='\\N', index=False, line_terminator='\n')
 
 # -------------------------------------------------------------------------------------------------
 # Names
@@ -114,15 +94,11 @@ names: pd.DataFrame = pd.read_csv(
   }
 )
 
-names = names[names['nconst'].isin(name_ids)]
-
 names.reset_index(drop=True, inplace=True)
 
 print('NAMES')
 print(names.head(5))
 line()
-
-names.to_csv(names_output, sep='\t', na_rep='\\N', index=False, line_terminator='\n')
 
 # -------------------------------------------------------------------------------------------------
 # Ratings
@@ -137,12 +113,8 @@ ratings: pd.DataFrame = pd.read_csv(
   }
 )
 
-ratings = ratings[ratings['tconst'].isin(title_ids)]
-
 ratings.reset_index(drop=True, inplace=True)
 
 print('RATINGS')
 print(ratings.head(5))
 line()
-
-ratings.to_csv(ratings_output, sep='\t', na_rep='\\N', index=False, line_terminator='\n')
