da8e5106b04ef506017f4fdfc90afd2478f9dcd1
Kaisa Korpisaari <kaisa.korpisaari@student.tut.fi>
2021-05-06 22:42:14 +0300

    Style bubble chart colours, texts etc

--- genre_bubble_chart.py
+++ genre_bubble_chart.py
@@ -31,18 +31,32 @@ def genre_bubble_chart(titles: pd.DataFrame):
   # Change circle objects to dicts for pandas
   circle_dicts = []
   circle_plot_width = 800
-  for c in circles:
-    circle_dicts.append({'x': c.x, 'y': c.y, 'r': c.r * circle_plot_width*0.9, 'genre': c.ex['genres']})
+  colors = brewer['Set3'][12]
+  for i, c in enumerate(circles):
+    circle_dicts.append({
+      'x': c.x,
+      'y': c.y,
+      'r': c.r * circle_plot_width*0.9,
+      'genre': c.ex['genres'],
+      'color': colors[i % len(colors)]
+    })
 
   # Plot the circles
   circles_df = pd.DataFrame(data=circle_dicts)
   circles_df.set_index(['genre'], inplace=True)
   circles_source = ColumnDataSource(circles_df)
   p = figure(x_range=[-1, 1], y_range=[-1, 1], plot_width=circle_plot_width, plot_height=circle_plot_width)
-  p.scatter(x='x', y='y', size='r', source=circles_source)
+
+  p.scatter(x='x', y='y', size='r', fill_color='color', line_color='color', source=circles_source)
 
   # To show the labels for each bubble
-  labels = LabelSet(x='x', y='y', text='genre', source=circles_source)
+  labels = LabelSet(x='x', y='y', text='genre', source=circles_source, text_align='center', text_baseline='middle')
   p.add_layout(labels)
+
+  p.xgrid.visible = False
+  p.ygrid.visible = False
+
+  p.xaxis.visible = False
+  p.yaxis.visible = False
 
   return p
