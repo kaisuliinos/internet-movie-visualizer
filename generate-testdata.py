dfbe424df3bbe67a1cc5651ad6df46903ed47fd8
Mika Kuitunen <mika.kuitunen@hotmail.com>
2021-05-06 15:02:09 +0300

    Add principals and names test data

--- generate-testdata.py
+++ generate-testdata.py
@@ -15,12 +15,16 @@ def formPaths(file):
   return (input, output)
 
 titles_file = 'title.basics'
-crew_file = 'title.crew'
+principals_file = 'title.principals'
+names_file = 'name.basics'
 
 titles_input, titles_output = formPaths(titles_file)
-crew_input, crew_output = formPaths(crew_file)
+principals_input, principals_output = formPaths(principals_file)
+names_input, names_output = formPaths(names_file)
 
-# Import titles
+# -------------------------------------------------------------------------------------------------
+# Titles
+
 titles: pd.DataFrame = pd.read_csv(
   titles_input,
   sep='\t',
@@ -37,33 +41,78 @@ titles: pd.DataFrame = pd.read_csv(
   }
 )
 
+print('TITLES')
+print('Title types:')
 print(titles.titleType.unique())
 
-categories = ['short', 'movie', 'tvShort', 'tvMovie', 'tvSeries', 'tvMiniSeries']
+categories = ['movie', 'tvMovie', 'tvSeries', 'tvMiniSeries']
 titles = titles[titles['titleType'].isin(categories)]
 
 titles = titles.sample(10000)
 titles.reset_index(drop=True, inplace=True)
 
+print(titles.head(5))
+line()
+
 titles.to_csv(titles_output, sep='\t', na_rep='\\N', index=False)
+
+# -------------------------------------------------------------------------------------------------
+# Principals
 
 title_ids = titles['tconst']
 
-# Crew
-crew: pd.DataFrame = pd.read_csv(
-  crew_input,
+principals: pd.DataFrame = pd.read_csv(
+  principals_input,
   sep='\t',
   dtype={
     'tconst': str,
-    'directors': str,
-    'writers': str
+    'ordering': int,
+    'nconst': str,
+    'category': str,
+    'job': str,
+    'characters': str
   }
 )
 
-crew = crew[crew['tconst'].isin(title_ids)]
-crew.reset_index(drop=True, inplace=True)
-
-print(crew.head(5))
+print('PRINCIPALS')
+print('Principal categories:')
+print(principals.category.unique())
 line()
 
-crew.to_csv(crew_output, sep='\t', na_rep='\\N', index=False)
+job_titles = ['self', 'director', 'cinematographer', 'composer', 'producer', 'actor', 'actress', 'writer']
+
+principals = principals[principals['category'].isin(job_titles)]
+principals = principals[principals['tconst'].isin(title_ids)]
+principals.reset_index(drop=True, inplace=True)
+
+print(principals.head(5))
+line()
+
+principals.to_csv(principals_output, sep='\t', na_rep='\\N', index=False)
+
+# -------------------------------------------------------------------------------------------------
+# Names
+
+name_ids = principals['nconst']
+
+names: pd.DataFrame = pd.read_csv(
+  names_input,
+  sep='\t',
+  dtype={
+    'nconst': str,
+    'primaryName': str,
+    'birthYear': str,
+    'deathYear': str,
+    'primaryProfession': str,
+    'knownForTitles': str
+  }
+)
+
+names = names[names['nconst'].isin(name_ids)]
+names.reset_index(drop=True, inplace=True)
+
+print('NAMES')
+print(names.head(5))
+line()
+
+names.to_csv(names_output, sep='\t', na_rep='\\N', index=False)
