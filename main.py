f027ac3efe5feda208d621d9876ac8cb21e5a15e
Kaisa Korpisaari <kaisa.korpisaari@student.tut.fi>
2021-05-11 14:37:57 +0300

    Add dummy slider for year selection

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
 from bokeh.plotting import show
-from bokeh.layouts import layout
+from bokeh.layouts import layout, column
+from bokeh.models import RangeSlider
 
 from read_data import read_names, read_principals, read_titles, read_ratings
 from genre_bubble_chart import genre_bubble_chart
@@ -18,10 +19,20 @@ names: pd.DataFrame = read_names()
 
 titles = titles.join(other=ratings, on='tconst', rsuffix='_ratings')
 
-p = genre_bubble_chart(titles.copy()) # Pass a copy instead of reference
-tl = top_list(titles.copy())
+bubble_chart = genre_bubble_chart(titles.copy()) # Pass a copy instead of reference
+toplist = top_list(titles.copy())
 
+year_slider = RangeSlider(
+  title="Select the release year range",
+  start=1910,
+  end=2021,
+  step=1,
+  value=(1910, 2021)
+)
+
+c = column(year_slider, toplist)
 lo = layout([
-  [p, tl]
+  [bubble_chart, c]
 ])
+
 show(lo)
